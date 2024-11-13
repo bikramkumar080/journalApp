376293f8668a3e37024ede9a83df099475627457
Bikram Kumar <bikramkumar080@gmail.com>
2024-11-13 10:13:14 +0530

    Controller class

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,42 +1,53 @@
 package net.engineeringdigest.journalApp.controller;
 
+
+import net.engineeringdigest.journalApp.Service.JournalEntryService;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
+
     @GetMapping
-    public List<JournalEntry> getJournalEntries() {
-        return new ArrayList<>(journalEntries.values());
+    public List<JournalEntry> journalEntries(){
+        return journalEntryService.getAllEntries();
     }
 
     @PostMapping
-    public JournalEntry createJournalEntry(@RequestBody JournalEntry journalEntry) {
-        journalEntries.put(journalEntry.getId(), journalEntry);
-        return journalEntry;
+    public void addJournalEntry(@RequestBody JournalEntry journalEntry){
+        journalEntryService.save(journalEntry);
     }
 
     @GetMapping("id/{id}")
-    public JournalEntry getJournalEntry(@PathVariable Long id) {
-        return journalEntries.get(id);
+    public JournalEntry getJournalEntryById(@PathVariable String id){
+        return journalEntryService.getEntryById(id).orElse(null);
     }
 
     @DeleteMapping("id/{id}")
-    public JournalEntry deleteJournalEntry(@PathVariable Long id) {
-        return journalEntries.remove(id);
+    public boolean deleteJournalEntryById(@PathVariable String id){
+        journalEntryService.deleteById(id);
+        return true;
     }
 
     @PutMapping("id/{id}")
-    public JournalEntry updateJournalEntry(@PathVariable Long id, @RequestBody JournalEntry journalEntry) {
-        journalEntries.put(id, journalEntry);
-        return journalEntry;
+    public JournalEntry updateJournalEntry(@PathVariable String id, @RequestBody JournalEntry newEntry) {
+        JournalEntry old = journalEntryService.getEntryById(id).orElse(null);
+        if (old != null) {
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
+            journalEntryService.save(old);
+            return old;
+        } else {
+            newEntry.setId(id);
+            journalEntryService.save(newEntry);
+            return newEntry;
+        }
     }
 }
